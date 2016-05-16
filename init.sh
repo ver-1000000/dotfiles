052fe4164e0246177a75e2b9a1eac9c87041d3ef
ShotaSASAKI <sasaki@ryunos.co.jp>
2016-05-16 10:24:49 +0900

    Rename mklink.sh -> init.sh, and add vimproc build code.

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@ mkdir -p ~/.vim/bundle
 mkdir -p ~/.tmux/plugins/tpm
 
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim &
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim &
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm &
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -O git-completion.bash &
 
@@ -19,4 +20,5 @@ git config --global push.default simple
 echo 'source ~/dotfiles/git-completion.bash' >> ~/.bashrc
 echo 'alias tmux="tmux -2"' >> ~/.bashrc
 
+cd ~/.vim/bundle/vimproc && make && cd
 exit
